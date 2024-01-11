818627067afe6da0be3192822c1f43f71cf85d6e
Chirag singhal <102845461+Chiragsinghal24@users.noreply.github.com>
2024-01-11 22:27:19 +0530

    Update Hero.jsx
    color change

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                                 <path fill="#F1B550" d="M16.9 13.849c.04.06.06.14.06.22 0 .08-.02.16-.06.24-.02.06-.08.12-.12.18l-4.12 4.103c-.1.12-.26.18-.4.18-.16 0-.3-.06-.42-.18L9.98 16.75c-.1-.1-.16-.26-.16-.42 0-.14.06-.301.16-.421.12-.1.28-.16.42-.16.16 0 .32.06.42.16l1 1a.607.607 0 0 0 .82 0l3.28-3.281c.06-.06.12-.1.2-.14.08-.02.16-.04.24-.04.08 0 .14.02.22.06.08.04.14.08.2.14.04.06.1.12.12.2Z" data-color="1"></path>
                                 <path fill="#F1B550" d="M25.86 6.064c-.08-.22-.22-.42-.38-.58L20.52.52c-.16-.16-.36-.3-.58-.38-.22-.1-.46-.14-.68-.14H1.78c-.24 0-.46.04-.68.14C.88.22.68.36.52.52.36.68.22.88.14 1.1a1.6 1.6 0 0 0-.14.681V30.24c0 .22.04.46.14.68.08.2.22.4.38.56.16.18.36.3.58.4.22.081.44.121.68.121H24.2c.48 0 .92-.18 1.26-.5.34-.32.52-.76.54-1.221V6.744c0-.24-.04-.46-.14-.68Zm-5.12 7.384c-.26 2.042-.88 4.003-1.88 5.784a12.364 12.364 0 0 1-5.14 5.043l-.36.18-.74.02-.96-.54a12.246 12.246 0 0 1-4.52-4.683 16.2 16.2 0 0 1-2-6.924c-.06-.5-.06-1 0-1.481 0-.1 0-.08.24-.1 1.88-.18 3.7-.76 5.34-1.681.76-.46 1.48-.96 2.18-1.501.02-.02.06-.04.08-.04.04 0 .06.02.08.04l.56.44c1.54 1.14 3.3 1.961 5.18 2.421.6.14 1.22.22 1.84.3.16 0 .22.08.22.22.04.841 0 1.682-.12 2.502Z" data-color="1"></path>
                             </g>
-                        </svg><Link to="/service">Plan Set</Link></button>
+                        </svg><Link to="/service">Plan Sets</Link></button>
                         <button className="btn btn-primary text-white"><svg preserveAspectRatio="xMidYMid meet" data-bbox="-0.001 0.5 20.002 32.999" xmlns="http://www.w3.org/2000/svg" viewBox="-0.001 0.5 20.002 32.999" height="34" width="20" data-type="color" role="presentation" aria-hidden="true" aria-label="">
                             <g>
                                 <path fill="#6158EF" d="M2.035 6.573 8.993.858a1.569 1.569 0 0 1 1.996 0l6.957 5.715c.387.319.62.837.62 1.354v10.554c0 .955-.736 1.712-1.628 1.712H3.043c-.891 0-1.628-.776-1.628-1.712V7.927c0-.517.233-1.015.62-1.354Z" data-color="1"></path>
@@ -45,7 +45,7 @@ const Hero = () => {
                                 <path fill="#BE9AFA" d="M0 2.526A2.532 2.532 0 0 1 2.537 0h17.926A2.532 2.532 0 0 1 23 2.526v26.948A2.532 2.532 0 0 1 20.463 32h-3.791l-.014-3.237h.689c.366 0 .689-.31.689-.704.021-.197-.065-.373-.237-.526l-5.5-4.923a.707.707 0 0 0-.452-.154c-.15 0-.323.022-.473.176l-5.521 4.9a.72.72 0 0 0-.216.527c0 .397.302.704.69.704h.69v1.526c-.002.02-.002.042-.002.062V32H2.537A2.532 2.532 0 0 1 0 29.474V2.526Zm5.022 12.182a.506.506 0 1 0 0 1.01h13.642a.506.506 0 1 0 0-1.01H5.022Zm-.508 3.873c0-.279.227-.505.508-.505h9.132a.506.506 0 1 1 0 1.01H5.022a.506.506 0 0 1-.508-.505Zm7.328-15.213a.221.221 0 0 0-.112.03L8.573 5.194a.225.225 0 0 0-.113.195v.324c0 .99.324 1.954.923 2.745.6.791 1.44 1.366 2.397 1.638.04.012.084.012.124 0a4.578 4.578 0 0 0 2.397-1.638c.6-.79.924-1.754.924-2.745v-.324a.225.225 0 0 0-.114-.195l-3.157-1.796a.226.226 0 0 0-.112-.03Z" clip-rule="evenodd" fill-rule="evenodd" data-color="1"></path>
                                 <path fill="#BE9AFA" d="M13.217 29.857V32H10.46v-2.143c0-.389.308-.702.689-.702h1.378c.381 0 .689.313.689.701Z" data-color="1"></path>
                             </g>
-                        </svg><Link to="/service">PE Stamp</Link></button>
+                        </svg><Link to="/service">PE Stamps</Link></button>
                         <button className="btn btn-primary text-white"><svg preserveAspectRatio="xMidYMid meet" data-bbox="0 0.5 34 31.001" xmlns="http://www.w3.org/2000/svg" viewBox="0 0.5 34 31.001" height="32" width="34" data-type="color" role="presentation" aria-hidden="true" aria-label="">
                             <g>
                                 <path fill="#78DBF3" d="m17 .5 5.276 6.315h-4.104A1.16 1.16 0 0 1 17 5.667V.5Z" data-color="1"></path>
@@ -62,7 +62,7 @@ const Hero = () => {
                     </div>
 
                     <div className='flex mt-10 w-full justify-center'>
-                        <button className="btn btn-primary rounded-3xl text-white">
+                        <button className="btn btn-warning rounded-3xl text-white">
                             <a href="https://docs.google.com/forms/d/1tms2OKyRGsV6bF8tFYqOkw0aGWsMjqF-3zCjJnloAOs/viewform?edit_requested=true" target="_blank">Submit Design Request Here!</a></button>
                     </div>
                 </div>
